e10ce425b239a5e9528283d2c53f742bf6a837f3
TakuyaHimeji <himeji@cs-reporters.com>
2017-12-25 01:33:09 +0900

    Firest Commit.

---
+++ Assets/CaptureScreen/Editor/CaptureCameraEditor.cs
@@ -0,0 +1,63 @@
+﻿// =================================
+//
+//	CaptureCameraEditor.cs
+//	Created by Takuya Himeji
+//
+// =================================
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CaptureCamera))]
+public class CaptureCameraEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        CaptureCamera captureCamera = target as CaptureCamera;
+
+		bool disabled = false;
+        if (captureCamera.transform.parent == null || captureCamera.transform.parent.GetComponent<Camera>() == null)
+        {
+			disabled = true;
+			captureCamera.inheritance = false;
+        }
+
+        EditorGUI.BeginDisabledGroup(disabled);
+		captureCamera.inheritance = EditorGUILayout.Toggle("Inheritance", captureCamera.inheritance);
+        EditorGUI.EndDisabledGroup();
+		if (disabled)
+		{
+            EditorGUILayout.HelpBox("親カメラが存在しません。", MessageType.Warning);
+		}
+		else if (captureCamera.inheritance)
+        {
+            EditorGUILayout.HelpBox("親カメラ(" + captureCamera.transform.parent.name + ")のパラメータを継承します。", MessageType.Info);
+        }
+
+        // 解像度の設定
+        captureCamera.resolution = EditorGUILayout.Popup(
+            "CaptureResolution",
+            captureCamera.resolution,
+            new string[] { "SD (720x480)", "HD (1280x720)", "FullHD (1920x1080)", "4K (4096x2160)", "8K (7680x4320)", "Free" }
+        );
+
+        if (captureCamera.resolution == 4)
+        {
+            EditorGUILayout.HelpBox("8K解像度の場合、PCのスペックによっては実行時にUnityが強制終了する場合があります。", MessageType.Warning);
+        }
+        else if (captureCamera.resolution == 5)
+        {
+            EditorGUILayout.Vector2IntField("Screen Size", new Vector2Int(captureCamera.width, captureCamera.height));
+            EditorGUILayout.Space();
+        }
+
+        // アンチエイリアスの設定
+        captureCamera.antiAiliasing = EditorGUILayout.Popup(
+            "Anti-Ailiasing",
+            captureCamera.antiAiliasing,
+            new string[] { "Disabled", "2 samples", "4 samples", "8 samples" }
+        );
+    }
+}
---
+++ Assets/CaptureScreen/Scripts/CaptureCamera.cs
@@ -0,0 +1,144 @@
+﻿// =================================
+//
+//	CaptureCamera.cs
+//	Created by Takuya Himeji
+//
+// =================================
+
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CaptureCamera : MonoBehaviour
+{
+	#region Inspector Settings
+	public int resolution		= 1;	// 解像度
+	public int antiAiliasing	= 0;	// アンチエイリアス
+	public bool inheritance		= true; // 親カメラのパラメータを継承するか
+	public int width	= 1280;
+	public int height	= 720;
+	#endregion // Inspector Settings
+
+
+	#region Member Field
+	private RenderTexture renderTexture; // 保存対象のRenderTexture
+	#endregion // Member Field
+
+
+	#region MonoBehaviour Methods
+	void Start ()
+	{
+		// カメラ取得
+		Camera cam = GetComponent<Camera> ();
+		// RenderTexture取得
+		renderTexture = cam.targetTexture;
+
+		// 解像度の設定
+		switch (resolution)
+		{
+			// SD
+            case 0:
+                renderTexture.width	= 720;
+                renderTexture.height = 480;
+                break;
+            // HD
+            case 1:
+                renderTexture.width = 1280;
+                renderTexture.height = 720;
+                break;
+			// FullHD
+			case 2:
+                renderTexture.width = 1920;
+                renderTexture.height = 1080;
+            	break;
+			// 4K
+			case 3:
+                renderTexture.width = 4096;
+                renderTexture.height = 2160;
+				break;
+			// 8K
+			case 4:
+                renderTexture.width = 7680;
+                renderTexture.height = 4320;
+				break;
+
+			// Free
+			case 5:
+                renderTexture.width = width;
+                renderTexture.height = height;
+				break;
+		}
+
+		// アンチエイリアスの設定
+		switch (antiAiliasing)
+		{
+			case 0:
+				renderTexture.antiAliasing = 0;
+				break;
+            case 1:
+                renderTexture.antiAliasing = 2;
+				break;
+            case 2:
+                renderTexture.antiAliasing = 4;
+            	break;
+            case 3:
+                renderTexture.antiAliasing = 8;
+                break;
+		}
+
+		// 親カメラの設定を引き継ぐ
+		if (inheritance) {
+			Camera parentCam	= transform.parent.GetComponent<Camera>();
+			cam.fieldOfView		= parentCam.fieldOfView;
+			cam.farClipPlane	= parentCam.farClipPlane;
+			cam.nearClipPlane	= parentCam.nearClipPlane;
+			cam.clearFlags		= parentCam.clearFlags;
+			cam.backgroundColor = parentCam.backgroundColor;
+			cam.aspect			= parentCam.aspect;
+			if (parentCam.orthographic)
+			{
+				cam.projectionMatrix = parentCam.projectionMatrix;
+			}
+		}
+	}
+	
+	void Update ()
+	{
+		
+	}
+
+	void OnGUI ()
+	{
+		if (GUI.Button(new Rect(10, 10, 150, 100), "Shoot!!!")) {
+			// 撮影
+			CaptureOnScreen ();
+		}
+	}
+	#endregion // MonoBehaviour Methods
+
+
+	#region Member Methods
+	// 撮影し、書き出す
+    public void CaptureOnScreen()
+    {
+		Texture2D tex = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGB24, false);
+        RenderTexture.active = renderTexture;
+        tex.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+        tex.Apply();
+ 
+        // pngテクスチャの生成
+        byte[] bytes = tex.EncodeToPNG();
+        Object.Destroy(tex);
+ 
+        // 指定のディレクトリへ保存
+		string saveDir = Application.dataPath + "/../Captures";
+		if (!Directory.Exists(saveDir))
+		{
+			Directory.CreateDirectory(saveDir);
+		}
+        File.WriteAllBytes(saveDir + "/SavedScreen_"+ System.DateTime.Now.ToString("yyyyMMddHHmmss") +".png", bytes);
+    }
+	#endregion // Member Methods
+}
